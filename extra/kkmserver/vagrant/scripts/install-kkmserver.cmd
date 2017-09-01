22c738ac9955f4f2d950c290c1f6bca2659bc105
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-09-01 04:54:46 +0300

    При установке сервиса в вирутальной машине отключи вывод на консоль и создай лог-файл

--- extra/kkmserver/vagrant/scripts/install-kkmserver.cmd
+++ extra/kkmserver/vagrant/scripts/install-kkmserver.cmd
@@ -5,7 +5,7 @@ set KKMSERVER_HOME="C:\kkmserver"
 echo Installing KkmServer...
 C:\vagrant\resources\Setup_KkmServer.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /LOG=c:\kkmserver-install.log /DIR=%KKMSERVER_HOME%
 copy C:\vagrant\resources\SettingsServ.ini %KKMSERVER_HOME%\Settings
-%KKMSERVER_HOME%\InstallUtil.exe %KKMSERVER_HOME%\KkmServer.exe
+%KKMSERVER_HOME%\InstallUtil.exe %KKMSERVER_HOME%\KkmServer.exe /LogFile=c:\kkmserver-service-install.log /LogToConsole=false
 net start KkmServer
 netsh advfirewall firewall add rule name="KkmServer" dir=in action=allow protocol=TCP localport=5893
 echo ...done
