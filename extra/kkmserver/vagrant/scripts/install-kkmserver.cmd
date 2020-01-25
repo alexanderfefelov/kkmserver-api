8a47285280519a4c493b4e2c765217d9b68ec47c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2020-01-26 02:42:08 +0300

    Fix Vagrant provisioner

--- extra/kkmserver/vagrant/scripts/install-kkmserver.cmd
+++ extra/kkmserver/vagrant/scripts/install-kkmserver.cmd
@@ -5,7 +5,8 @@ set KKMSERVER_HOME="C:\kkmserver"
 echo Installing KkmServer...
 C:\vagrant\resources\Setup_KkmServer.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /LOG=c:\kkmserver-install.log /DIR=%KKMSERVER_HOME%
 copy C:\vagrant\resources\SettingsServ.ini %KKMSERVER_HOME%\Settings
-%KKMSERVER_HOME%\InstallUtil.exe %KKMSERVER_HOME%\KkmServer.exe /LogFile=c:\kkmserver-service-install.log /LogToConsole=false
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe %KKMSERVER_HOME%\KkmServer.exe /LogFile=c:\kkmserver-service-install.log /LogToConsole=false
+sc config KkmServer binpath= "%KKMSERVER_HOME%\KkmServer.exe RunAsService"
 net start KkmServer
 netsh advfirewall firewall add rule name="KkmServer" dir=in action=allow protocol=TCP localport=5893
 echo ...done
